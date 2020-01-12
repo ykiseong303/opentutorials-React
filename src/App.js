0b49828a0b395eadabfebc627a6872205b688ead
ykiseong303 <41833412+ykiseong303@users.noreply.github.com>
2020-01-12 16:14:18 +0900

    Using custom component in App.js

--- src/App.js
+++ src/App.js
@@ -1,51 +1,22 @@
 import React, { Component } from 'react';
+// 현재 디렉토리 아래에 componenet 디렉토리의 TOC.js 파일 import
+import TOC from "./components/TOC";
+import Content from "./components/Content";
+import Subject from "./components/Subject";
 import './App.css';
 
-//컴포넌트를 만들때는 return 문안에 반드시 하나의 최상위 태그로 시작해야함
-class Subject extends Component{
-  render(){
-    return(
-      <header>
-        <h1>WEB</h1>
-        world wide web!!
-      </header>
-    );
-  }
-}
-// TOC : Table Of Content
-class TOC extends Component{
-  render() {
-    return(
-      <nav>
-            <ul>
-                <li><a href="1.html">HTML</a></li>
-                <li><a href="2.html">CSS</a></li>
-                <li><a href="3.html">JavaScript</a></li>
-            </ul>
-      </nav>
-    );
-  }
-}
 
-class Content extends Component{
-  render(){
-    return(
-      <article>
-            <h2>HTML</h2>
-            HTML is Hyper Text Markup Language.
-      </article>
-    );
-  }
-}
+
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Subject></Subject>
+        <Subject title="WEB" sub="world wide web!!"></Subject>
         <TOC></TOC>
-        <Content></Content>
-
+        <Content title="HTML" desc="HTML is Hyper Text Markup Language."></Content>
       </div>
     );
   }
