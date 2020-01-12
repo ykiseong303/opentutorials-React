f7a0fe7e514161f14d4458ee7513d6c210f1cbd2
ykiseong303 <41833412+ykiseong303@users.noreply.github.com>
2020-01-12 15:35:58 +0900

    second day(1) study

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+//컴포넌트를 만들때는 return 문안에 반드시 하나의 최상위 태그로 시작해야함
+class Subject extends Component{
+  render(){
+    return(
+      <header>
+        <h1>WEB</h1>
+        world wide web!!
       </header>
-    </div>
-  );
+    );
+  }
+}
+// TOC : Table Of Content
+class TOC extends Component{
+  render() {
+    return(
+      <nav>
+            <ul>
+                <li><a href="1.html">HTML</a></li>
+                <li><a href="2.html">CSS</a></li>
+                <li><a href="3.html">JavaScript</a></li>
+            </ul>
+      </nav>
+    );
+  }
+}
+
+class Content extends Component{
+  render(){
+    return(
+      <article>
+            <h2>HTML</h2>
+            HTML is Hyper Text Markup Language.
+      </article>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Subject></Subject>
+        <TOC></TOC>
+        <Content></Content>
+
+      </div>
+    );
+  }
 }
 
 export default App;
